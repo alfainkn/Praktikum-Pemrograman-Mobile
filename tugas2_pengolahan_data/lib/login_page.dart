4b3ec7e95feb688618d2d7adb94a640c4ffaaf5d
alfainkn <alfainkn@fgmail.com>
2022-03-08 19:08:56 +0700

    tugas

---
+++ tugas2_pengolahan_data/lib/login_page.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String username = "";
+  String password = "";
+  bool isLoginSuccess = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ),
+        body: Column(children: 
+        [
+          _picture(),
+          _username(),
+          _password(),
+          _loginButton(context),
+        ]),
+      ),
+    );
+  }
+
+  Widget _picture() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 145, 0, 40),
+      alignment: Alignment.center,
+      child:  Image.asset('asset/images/flutter2.png', width:100,height:100));
+  }
+
+  Widget _username() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: TextFormField(
+        enabled: true,
+        onChanged: (value) {
+          username = value;
+        },
+        decoration: InputDecoration(
+          hintText: 'Username',
+          contentPadding: const EdgeInsets.all(8.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            borderSide:
+                BorderSide(color: (isLoginSuccess) ? Colors.blue : Colors.red),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _password() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: TextFormField(
+        enabled: true,
+        onChanged: (value) {
+          password = value;
+        },
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          contentPadding: const EdgeInsets.all(8.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            borderSide:
+                BorderSide(color: (isLoginSuccess) ? Colors.blue : Colors.red),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      width: MediaQuery.of(context).size.width,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: (isLoginSuccess) ? Colors.blue : Colors.red,
+          onPrimary: Colors.white,
+        ),
+        onPressed: () {
+          String text = "";
+          if (username == "alfain" && password == "1234") {
+            setState(() {
+              text = "Login Success";
+              isLoginSuccess = true;
+            });
+          } else {
+            setState(() {
+              text = "Login Failed";
+              isLoginSuccess = false;
+            });
+          }
+          SnackBar snackBar = SnackBar(
+            content: Text(text),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: const Text('Login'),
+      ),
+    );
+  }
+}
